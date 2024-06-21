f0f03e3849f38e7d67163467720fb10b82626e12
PhamTuanAnh2311 <tuananhjeep2311@gmail.com>
2024-06-21 12:02:23 +0700

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace PDTABaiThiLai1.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
